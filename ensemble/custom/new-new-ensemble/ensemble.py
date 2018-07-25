462eae1c88bc01a00742f678393fc7d7849d7b47
Michael Ryan <michael_ryan_2000@yahoo.com>
2018-07-25 03:04:34 -0400

    Hallelujah
    I don't typically think of myself as very religious, but this commit made me question that - a true miracle that this worked

--- ensemble/custom/new-new-ensemble/ensemble.py
+++ ensemble/custom/new-new-ensemble/ensemble.py
@@ -1,19 +1,20 @@
-import numpy as np
-import os
-import six.moves.urllib as urllib
-import sys
-import tarfile
-import tensorflow as tf
-import zipfile
-import cv2
-
-from collections import defaultdict
-from io import StringIO
-from matplotlib import pyplot as plt
-from PIL import Image
-
 from prediction import Box
 from prediction import Prediction
+
+from matplotlib import pyplot as plt
+import tensorflow as tf
+import numpy as np
+import cv2
+import sys
+
+import rfcn
+import classifier
+import detector2
+
+PATH_TO_IMAGE = 'C:/Users/micha/OneDrive/Documents/GitHub/CVML-GSET-Project/dataset/test-images/test1.jpg'
+PATH_TO_LABELS = 'C:/Users/micha/OneDrive/Documents/GitHub/CVML-GSET-Project/dataset/new-tensorflow-dataset/data/label_map.pbtxt'
+NUM_CLASSES = 8
+IMAGE_SIZE = (12, 8)
 
 # This is needed since the notebook is stored in the object_detection folder.
 sys.path.append("C:\\Users\\micha\\OneDrive\\Documents\\GitHub\\models\\research")
@@ -26,90 +27,46 @@ if tf.__version__ < '1.4.0':
 from utils import label_map_util
 from utils import visualization_utils as vis_util
 
-# Path to frozen detection graph. This is the actual model that is used for the object detection.
-PATH_TO_CKPT = 'C:/Users/micha/OneDrive/Documents/GitHub/CVML-GSET-Project/detector-models/rfcnmodel/models/rfcn/export/frozen_inference_graph.pb'
-# List of the strings that is used to add correct label for each box.
-PATH_TO_LABELS = 'C:/Users/micha/OneDrive/Documents/GitHub/CVML-GSET-Project/dataset/new-tensorflow-dataset/data/label_map.pbtxt'
-NUM_CLASSES = 8
-# Size, in inches, of the output images.
-IMAGE_SIZE = (12, 8)
-CONFIDENCE_THRESH = 0.5
-
-# Load tensorflow model
-detection_graph = tf.Graph()
-with detection_graph.as_default():
-    od_graph_def = tf.GraphDef()
-    with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
-        serialized_graph = fid.read()
-        od_graph_def.ParseFromString(serialized_graph)
-        tf.import_graph_def(od_graph_def, name='')
-
 # Load label map
 label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
 categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
 category_index = label_map_util.create_category_index(categories)
 
-def run_inference_for_single_image(image, graph):
-    with graph.as_default():
-        with tf.Session() as sess:
-            # Get handles to input and output tensors
-            ops = tf.get_default_graph().get_operations()
-            all_tensor_names = {output.name for op in ops for output in op.outputs}
-            tensor_dict = {}
-            for key in ['num_detections', 'detection_boxes', 'detection_scores', 'detection_classes', 'detection_masks']:
-                tensor_name = key + ':0'
-                if tensor_name in all_tensor_names:
-                    tensor_dict[key] = tf.get_default_graph().get_tensor_by_name(tensor_name)
-            if 'detection_masks' in tensor_dict:
-                # The following processing is only for single image
-                detection_boxes = tf.squeeze(tensor_dict['detection_boxes'], [0])
-                detection_masks = tf.squeeze(tensor_dict['detection_masks'], [0])
-                # Reframe is required to translate mask from box coordinates to image coordinates and fit the image size.
-                real_num_detection = tf.cast(tensor_dict['num_detections'][0], tf.int32)
-                detection_boxes = tf.slice(detection_boxes, [0, 0], [real_num_detection, -1])
-                detection_masks = tf.slice(detection_masks, [0, 0, 0], [real_num_detection, -1, -1])
-                detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(detection_masks, detection_boxes, image.shape[0], image.shape[1])
-                detection_masks_reframed = tf.cast(tf.greater(detection_masks_reframed, 0.5), tf.uint8)
-                # Follow the convention by adding back the batch dimension
-                tensor_dict['detection_masks'] = tf.expand_dims(detection_masks_reframed, 0)
-            image_tensor = tf.get_default_graph().get_tensor_by_name('image_tensor:0')
+def display_prediction(image, prediction):
+    vis_util.visualize_boxes_and_labels_on_image_array(image, np.array(prediction.get_coords()),
+      prediction.get_class_labels(), prediction.get_confidences(),
+      category_index, use_normalized_coordinates=True,
+      line_thickness=8)
+    plt.figure(figsize=IMAGE_SIZE)
+    plt.imshow(image)
+    plt.show()
 
-            # Run inference
-            output_dict = sess.run(tensor_dict, feed_dict={image_tensor: np.expand_dims(image, 0)})
+final_prediction = Prediction()
 
-            # all outputs are float32 numpy arrays, so convert types as appropriate
-            output_dict['num_detections'] = int(output_dict['num_detections'][0])
-            output_dict['detection_classes'] = output_dict['detection_classes'][0].astype(np.uint8)
-            output_dict['detection_boxes'] = output_dict['detection_boxes'][0]
-            output_dict['detection_scores'] = output_dict['detection_scores'][0]
-            if 'detection_masks' in output_dict:
-                output_dict['detection_masks'] = output_dict['detection_masks'][0]
-    return output_dict
+image = cv2.cvtColor(cv2.imread(PATH_TO_IMAGE), cv2.COLOR_BGR2RGB)
+prediction_one = rfcn.predict_img(image, False)
+for box in prediction_one.get_boxes():
+    new_img = box.splice_img(image)
+    prediction_n = detector2.predict_img(new_img, False)
+    matches = 0
+    confidences = []
+    for mini_box in prediction_n.get_boxes():
+        class_prediction = classifier.predict_img(mini_box.splice_img(new_img), False)
+        confidences.append(class_prediction.get_confidence())
+        if(mini_box.class_label == class_prediction.class_label):
+            matches += 1
+    if(matches == len(prediction_n.get_boxes())):
+        i = 0
+        for mini_box in prediction_n.get_boxes():
+            box_coords = box.get_coordinates_absolute(image)
+            mini_box_coords = mini_box.get_coordinates_absolute(new_img)
+            im_height = image.shape[0]
+            im_width = image.shape[1]
+            true_box = Box((box_coords[0] + mini_box_coords[0])/im_height, (box_coords[1] + mini_box_coords[1])/im_width,
+              (box_coords[0] + mini_box_coords[2])/im_height, (box_coords[1] + mini_box_coords[3])/im_width, confidences[i], mini_box.get_class_label())
+            final_prediction.append_box(true_box)
+            i += 1
+    else:
+        final_prediction.append_box(box)
 
-def process_img(image_np, display):
-    # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
-    image_np_expanded = np.expand_dims(image_np, axis=0)
-    # Actual detection.
-    output_dict = run_inference_for_single_image(image_np, detection_graph)
-    # Visualization of the results of a detection.
-    if(display):
-        vis_util.visualize_boxes_and_labels_on_image_array(image_np, output_dict['detection_boxes'],
-          output_dict['detection_classes'], output_dict['detection_scores'],
-          category_index, instance_masks=output_dict.get('detection_masks'),
-          use_normalized_coordinates=True, line_thickness=8)
-        plt.figure(figsize=IMAGE_SIZE)
-        plt.imshow(image_np)
-        plt.show()
-
-    return output_dict
-
-def predict_img(image, display):
-    output_dict = process_img(image, display)
-    prediction = Prediction()
-    for i in range(len(output_dict['detection_boxes'])) :
-        if(output_dict['detection_scores'][i] >= CONFIDENCE_THRESH):
-            detection_boxes = (output_dict['detection_boxes'][i])
-            new_box = Box(detection_boxes[0], detection_boxes[1], detection_boxes[2],
-                detection_boxes[3], output_dict['detection_scores'][i], output_dict['detection_classes'][i])
-            prediction.append_box(new_box)
-    return prediction
+display_prediction(image, final_prediction)
---
+++ ensemble/custom/remake-ensemble/classifier.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+from prediction import Class_Prediction
+
+def predict_img(image, display):
+    return Class_Prediction(1.0, 1)
